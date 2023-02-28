e90822d90b44d9fe25594ad0dc02bf5c7952ee73
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-28 16:39:48 +0700

    API: PATCH request

--- src/routes/api/comments/[commentId]/+server.ts
+++ src/routes/api/comments/[commentId]/+server.ts
@@ -4,9 +4,17 @@ import { comments } from "$lib/comments";
 export const GET = (requestEvent: any) => {
     const { params } = requestEvent;
     const { commentId } = params;
-    console.log("Hello ", commentId);
-    
     const comment = comments.find((comment: { id: string, text: string }) => comment.id == commentId);
     
+    return json(comment);
+}
+
+export const PATCH = async (requestEvent: any) => {
+    const { params, request } = requestEvent;
+    const { commentId } = params;
+    const { text } = await request.json();
+    const comment = comments.find((comment: { id: string, text: string }) => comment.id == commentId);
+    comment.text = text;
+
     return json(comment);
 }
