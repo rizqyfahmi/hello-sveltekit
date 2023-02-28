b485f114e53a35ce82a12f0c01602483741b67ce
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-01 00:29:12 +0700

    Data: using URL params

---
+++ src/routes/products/[productId]/+page.server.ts
@@ -0,0 +1,8 @@
+export const load = async ({ fetch, params }) => {
+    const { productId } = params;
+    const title = 'Product details';
+    const notification = 'End of season sale! 60% off!';
+    const response = await fetch(`http://localhost:4000/products/${productId}`);
+    const product = await response.json();
+    return { title, notification, product };
+};
