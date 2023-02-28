ee72cdc04876f26310dd49c2c1a680b58e068e4f
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-01 00:40:07 +0700

    Data: errors

--- src/routes/products/[productId]/+page.server.ts
+++ src/routes/products/[productId]/+page.server.ts
@@ -1,8 +1,15 @@
+import { error } from '@sveltejs/kit';
+
 export const load = async ({ fetch, params }) => {
     const { productId } = params;
+    if (productId > 3) {
+        throw error(404, { message: 'Product not found' });
+    }
     const title = 'Product details';
     const notification = 'End of season sale! 60% off!';
     const response = await fetch(`http://localhost:4000/products/${productId}`);
     const product = await response.json();
     return { title, notification, product };
 };
+
+export const prerender = 'auto';
