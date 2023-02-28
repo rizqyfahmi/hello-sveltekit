137b4c101cc0fad694ac3a000a9726af40fa0c59
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-28 11:49:51 +0700

    Routing: route navigation programatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+
+    const onClick = () => {
+        goto("/products")
+    }
+</script>
+
 <h1>Welcome home!</h1>
 
 <a href="/blog">Blog</a>
 <a href="/products">Products</a>
+
+<button on:click={onClick}>Place Order</button>
