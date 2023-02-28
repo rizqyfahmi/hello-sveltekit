0ff6edac6787a41ec2ea4bd6c3bb333ba5274ab5
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-28 13:42:05 +0700

    Routing: route layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<div class="layout-header">Header</div>
+<slot />
+<div class="layout-footer">Footer</div>
+
+<style>
+    .layout-header {
+        background-color: #E96479;
+        padding: 12px;
+    }
+
+    .layout-footer {
+        background-color: #4D455D;
+        padding: 12px;
+    }
+</style>
