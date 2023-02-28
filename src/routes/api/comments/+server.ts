8d24d524b4c3900f6fcf3da4a7e01350f3cb4cfa
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-28 16:21:14 +0700

    API: POST request

--- src/routes/api/comments/+server.ts
+++ src/routes/api/comments/+server.ts
@@ -4,3 +4,15 @@ import { comments } from "$lib/comments"
 export const GET = () => {
     return json(comments)
 }
+
+export const POST = async (requestEvent: any) => {
+    const { request } = requestEvent;
+    const { text } = await request.json();
+    const newComment = {
+        id: comments.length + 1,
+        text
+    };
+
+    comments.push(newComment);
+    return json(newComment, { status: 201 })
+}
