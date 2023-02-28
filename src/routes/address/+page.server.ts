45dd572075da6b7668888d4470e0f12b1d23e30d
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-01 00:05:14 +0700

    Data: server load function

--- src/routes/address/+page.server.ts
+++ src/routes/address/+page.server.ts
@@ -1,6 +1,6 @@
-export const load = async (loadEvent: any) => {
-    console.log("Load function called in page.ts");
-    const { fetch } = loadEvent;
+export const load = async (serverLoadEvent: any) => {
+    console.log("Load function called in page.server.ts");
+    const { fetch } = serverLoadEvent;
     const response = await fetch("api/postcodes");
     const postcodes = await response.json();
     const addressList = postcodes.map((postcode: any) => {
