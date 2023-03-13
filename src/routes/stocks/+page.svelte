0b7cbb98c00849f83a6a1ca173ddf7e5cba1269e
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-13 18:04:07 +0700

    Data: Before promise unwrapping

---
+++ src/routes/stocks/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let data;
+</script>
+
+<span>Most active stock - {data.mostActiveStock.symbol}</span>
+<span>Top gaining stock - {data.topGainingStock.symbol}</span>
+<span>Top losing stock - {data.topLosingStock.symbol}</span>
+
+<style>
+    span {
+        margin-right: 30px;
+        font-size: 24px;
+    }
+</style>
