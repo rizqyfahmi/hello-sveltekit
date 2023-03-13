a9613088c4c72121d88838cacccc83f5e40618e7
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-13 17:49:35 +0700

    Data: Using child data
    Display data from child

--- src/routes/products/+layout.ts
+++ src/routes/products/+layout.ts
@@ -3,10 +3,12 @@ export const load = async ({ fetch, parent }) => {
     const title = "Featured products";
     const response = await fetch("http://localhost:4000/featured-products");
     const featuredProducts = await response.json();
+    const notification = "End of season sale!"
     
     return {
         title,
         featuredProducts,
-        username
+        username,
+        notification
     }
 }
