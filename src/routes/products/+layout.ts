6f4625f59e146bb49091e552b34fb6537aa55501
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-13 17:31:03 +0700

    Data: Layout data

---
+++ src/routes/products/+layout.ts
@@ -0,0 +1,10 @@
+export const load = async ({ fetch }) => {
+    const title = "Featured products";
+    const response = await fetch("http://localhost:4000/featured-products");
+    const featuredProducts = await response.json();
+    
+    return {
+        title,
+        featuredProducts
+    }
+}
