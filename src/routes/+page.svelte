58113bc596c8776c260c20e41af04799313d747b
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-14 18:09:12 +0700

    Data: Preload code and data for specific link programatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,20 @@
+<script>
+
+	import { goto, preloadData } from "$app/navigation";
+
+</script>
 <h1 class="tall">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <!-- As the default when we click the link below, in the next page will scroll to the top. Using "data-sveltekit-noscroll", We can prevent it -->
 <a href="/products" data-sveltekit-noscroll>Products</a>
-
+<button 
+on:focus={async () => {
+    await preloadData("/products");
+}}
+on:mouseover={async () => {
+    await preloadData("/products");
+}}
+on:click={() => goto("/products")}>Go to products</button>
 <style>
     .tall {
         height: 100vh;
