f884bebd9f77626058b921c809e0e35a3a2142ba
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-14 17:22:27 +0700

    Data: Reload page when click a link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/products" data-sveltekit-preload-code="hover">Products</a>
+<!-- As the default when we click the link below, sveltekit will make it move page without refresh but "data-sveltekit-reload" we can make it refresh page when we click link below  -->
+<a href="/products" data-sveltekit-reload>Products</a>
