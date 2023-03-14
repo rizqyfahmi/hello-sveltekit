2d598f57db6caa92a8c946202d458a3a878822ba
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-14 18:10:34 +0700

    Data: Preload only code for specific link programmatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 
-	import { goto, preloadData } from "$app/navigation";
+	import { goto, preloadCode } from "$app/navigation";
 
 </script>
 <h1 class="tall">Welcome to SvelteKit</h1>
@@ -9,10 +9,10 @@
 <a href="/products" data-sveltekit-noscroll>Products</a>
 <button 
 on:focus={async () => {
-    await preloadData("/products");
+    await preloadCode("/products");
 }}
 on:mouseover={async () => {
-    await preloadData("/products");
+    await preloadCode("/products");
 }}
 on:click={() => goto("/products")}>Go to products</button>
 <style>
