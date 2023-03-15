818051883e2ead7a3dd1e7b6169912727797ff6e
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-15 09:26:37 +0700

    Page Options: SSR and CSR

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { page } from "$app/stores"
+    export let data;
+
+    const { username } = data;
+</script>
+{#if $page.data.notification}
+    <p>{ $page.data.notification }</p>
+{/if}
+<div>Welcome, { username }</div>
+<slot />
