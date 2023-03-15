9d02575b617b47e9d04f6282e3ee1588faeaafd0
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-15 11:21:18 +0700

    Page Options: Appying pre-rendered API routes into a page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,10 @@
-export const load = async () => {
+export const load = async ({ fetch }) => {
     console.log("Home page universal load function called");
+    const response = await fetch("/api/current-time");
+    const currentTime = await response.text();
+    return { currentTime };
 }
 
+// When a page set prerender to true than automatically makes the API route prerender set to true
 export const prerender = true;
 export const csr = false; // As the default "load" function will be called when we use csr, so that we need to set "csr" to be false in order to make sure that prerender is working well
